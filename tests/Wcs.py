00a3804724e7fbeed131c6939fa226062b240eda
fergal <fergal@git.lsstcorp.org>
2009-01-09 21:47:21 +0000

    Added test of finding GD66

--- tests/Wcs.py
+++ tests/Wcs.py
@@ -34,6 +34,40 @@ class WCSTestCaseNet(unittest.TestCase):
         """Test that tearDown does"""
         pass
 
+    def testFindGD66(self):
+	"""Pass the positions of objects near the white dwarf GD66 and test that the correct
+	position is returned
+
+	"""
+
+	gas = net.GlobalAstrometrySolution("backend.cfg")
+	starlist = loadXYFromFile("gd66xy.txt")
+	gas.setStarList(starlist)
+
+	#To speed the test, tell the GAS what the size of the image is
+	#The image is 1780 pixels on a side and covers half a square degree on the sky
+	gas.setImageScaleArcsecPerPixel(.5*3600/1780.)
+
+	flag = gas.blindSolve()
+
+	if flag:
+	    radec = gas.xy2RaDec(890, 890)
+
+	    #RA
+	    delta = math.fabs(radec[0]-80.159783)
+	    if delta > 1e-6:
+		return False
+
+	    #Dec
+	    delta = math.fabs(radec[1]-30.805249)
+	    if delta > 1e-6:
+		return False
+
+	    return True
+	else:
+	    #If we didn't get a match, that's a failure
+	    return False
+	    
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 def suite():
