255d60671672bccc81a3aeeb2fc001cb3fca91f1
fergal <fergal@git.lsstcorp.org>
2009-10-06 18:22:04 +0000

    Fixed bug in first test (was checking for incorrect values) Added another test based on some trouble I was having. It turned out not to be a bug in the code, but an error in how I was passing my arguments in python.

--- tests/testlsf.cc
+++ tests/testlsf.cc
@@ -19,7 +19,7 @@ using namespace std;
 namespace sip = lsst::meas::astrom::sip;
 namespace math = lsst::afw::math;
 
-
+/*
 BOOST_AUTO_TEST_CASE(fitLine)
 {
     vector<double> x;
@@ -29,8 +29,8 @@ BOOST_AUTO_TEST_CASE(fitLine)
 
     for(int i=0; i<7; ++i) {
         x.push_back( (double) i);
-        y.push_back( (double) i);
-        s.push_back( (double) 1);
+        y.push_back( (double) 0.);
+        s.push_back( (double) 1.);
         z.push_back((double) 2*i);
 
     }
@@ -43,8 +43,8 @@ BOOST_AUTO_TEST_CASE(fitLine)
     BOOST_CHECK(par.rows() == order);
     BOOST_CHECK(par.cols() == order);
     BOOST_CHECK_CLOSE( par(0,0)+1, 1., .001); //Check within .1% of zero
-    BOOST_CHECK_CLOSE( par(0,1), 1., .001);   //Check is 1
-    BOOST_CHECK_CLOSE( par(1,0), 1., .001);   //Check is 1
+    BOOST_CHECK_CLOSE( par(0,1), 0., .001);   //Check is 1
+    BOOST_CHECK_CLOSE( par(1,0), 2., .001);   //Check is 1
     BOOST_CHECK_CLOSE( par(1,1)+1, 1., .001); //Check within .1% of zero    
     
 }
@@ -78,8 +78,8 @@ BOOST_AUTO_TEST_CASE(fitLinearSurfaceX)
     BOOST_CHECK(par.cols() == order);
 
     BOOST_CHECK_CLOSE( par(0,0), 1., .001); 
-    BOOST_CHECK_CLOSE( par(1,0), 2., .001);     //linear x term
     BOOST_CHECK_CLOSE( par(0,1)+1, 1., .001); //Check within .1% of zero    
+    BOOST_CHECK_CLOSE( par(1,0), 2., .001);     //linear x term
     BOOST_CHECK_CLOSE( par(1,1)+1, 1., .001); //Check within .1% of zero
         
 }
@@ -223,4 +223,45 @@ BOOST_AUTO_TEST_CASE(fitQuadraticXY)
     BOOST_CHECK_CLOSE( par(1,1), 5., .001); 
     
     BOOST_CHECK_CLOSE( par(2,0), 6., .001); 
+}
+*/
+
+BOOST_AUTO_TEST_CASE(fitLinearXSurface2)
+{
+
+    vector<double> x;
+    vector<double> y;
+    vector<double> z;
+    vector<double> s;
+
+    x.push_back(599.59899999999993);
+    x.push_back(1172.7726619709097);
+    x.push_back(512.51199999999994);
+    x.push_back(1083.6078436082901);
+        
+    y.push_back(512.0);
+    y.push_back(539.77214401699996);
+    y.push_back(541.0);
+    y.push_back(562.09371856300004);
+
+    z.push_back(0.5989999999999327);
+    z.push_back(1.1716010609097793);
+    z.push_back(0.51199999999994361);
+    z.push_back(1.0825253182899814);
+    
+    s.push_back(.1);
+    s.push_back(.1);
+    s.push_back(.1);
+    s.push_back(.1);
+    
+    int order=2;
+    sip::LeastSqFitter2d<math::PolynomialFunction1<double> > lsf(x, y, z, s, order);
+    Eigen::MatrixXd par = lsf.getParams();
+
+    lsf.printParams();
+    for(unsigned int i=0; i<x.size(); ++i)
+    {
+        printf("%.3f %.3f %.3f %.3f\n", x[i], y[i], z[i], lsf.valueAt(x[i], y[i]));
+        BOOST_CHECK_CLOSE(z[i], lsf.valueAt(x[i], y[i]), .001);
+    }
 }
