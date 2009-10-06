bb662b0ecb5fb0363349356ab57224e65ea56961
fergal <fergal@git.lsstcorp.org>
2009-10-06 18:20:13 +0000

    Uncommented tests, changed name of test.

--- tests/lsf1d.py
+++ tests/lsf1d.py
@@ -16,7 +16,7 @@ import lsst.pex.exceptions.exceptionsLib as exception
 import lsst.meas.astrom.sip as sip
 
 
-class WCSTestCaseNet(unittest.TestCase):
+class Lsf1dTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
@@ -73,48 +73,47 @@ class WCSTestCaseNet(unittest.TestCase):
         self.assertRaises(exception.LsstCppException,
             sip.LeastSqFitter1dPoly, x, y,s,order )
 
-    #def testConst1(self):
-        #"""Check that code fits a dc offset correctly (1)"""
-        #
-        #x = [630, 1000, 1205, 1427]
-        #y = [.622, 1.109, 1.198, 1.429]
-        #s = [1,1,1,1]
-        #
-        #order=1
-#
-        #lsf = sip.LeastSqFitter1dPoly(x, y,s,order)
-        #
-        #for i in range(1, len(x)):
-            #self.assertAlmostEqual(lsf.valueAt(x[0]), lsf.valueAt(x[i]), 4)
-            ##print x[i], y[i], lsf.valueAt(x[i])
- #       
+    def testConst1(self):
+        """Check that code fits a dc offset correctly (1)"""
+        
+        x = [630, 1000, 1205, 1427]
+        y = [.622, 1.109, 1.198, 1.429]
+        s = [1,1,1,1]
+        
+        order=1
 
-    #def testCompareToCpp1(self):
-        #"""Confirm that I get the same behaviour in Python as I 
-        #do in the C++ test fitLinear3
-        #"""
-        #
-        ##This test arises because I was having trouble using Eigen's
-        ##svd inversion. fitLinear3 in C++ worked fine, but this test
-        ##behaved differently, and I tracked the result down to
-        ##the svd routines. 
-        #
-        #x = [689.301136505, 1112.8573687, 1386.67168477]
-        #y = [0.66911456573, 1.1147439759, 1.39597284177]
-        #s = [1,1,1]
-        #
-        #order=2
-        #print
-        #lsf = sip.LeastSqFitter1dPoly(x, y,s,order)
-#
-        #self.assertAlmostEqual(lsf.valueAt(x[0]), 0.670187891961, 4 )
-        #self.assertAlmostEqual(lsf.valueAt(x[1]), 1.11201034929, 4 )
-        #self.assertAlmostEqual(lsf.valueAt(x[2]), 1.39763314215, 4 )
+        lsf = sip.LeastSqFitter1dPoly(x, y,s,order)
+        
+        for i in range(1, len(x)):
+            self.assertAlmostEqual(lsf.valueAt(x[0]), lsf.valueAt(x[i]), 4)
+            #print x[i], y[i], lsf.valueAt(x[i])
+        
+
+    def testCompareToCpp1(self):
+        """Confirm that I get the same behaviour in Python as I 
+        do in the C++ test fitLinear3
+        """
+        
+        #This test arises because I was having trouble using Eigen's
+        #svd inversion. fitLinear3 in C++ worked fine, but this test
+        #behaved differently, and I tracked the result down to
+        #the svd routines. 
+        
+        x = [689.301136505, 1112.8573687, 1386.67168477]
+        y = [0.66911456573, 1.1147439759, 1.39597284177]
+        s = [1,1,1]
+        
+        order=2
+        print
+        lsf = sip.LeastSqFitter1dPoly(x, y,s,order)
+
+        self.assertAlmostEqual(lsf.valueAt(x[0]), 0.670187891961, 4 )
+        self.assertAlmostEqual(lsf.valueAt(x[1]), 1.11201034929, 4 )
+        self.assertAlmostEqual(lsf.valueAt(x[2]), 1.39763314215, 4 )
         
 
     def testCompareToCpp2(self):
-        """Confirm that I get the same behaviour in Python as I 
-        do in the C++ test fitLinear3
+        """Confirm that I get the same behaviour in Python as I do in the C++ test fitLinear3
         """
         
         #This test arises because I was having trouble using Eigen's
@@ -147,7 +146,7 @@ def suite():
     utilsTests.init()
 
     suites = []
-    suites += unittest.makeSuite(WCSTestCaseNet)
+    suites += unittest.makeSuite(Lsf1dTestCase)
     suites += unittest.makeSuite(utilsTests.MemoryTestCase)
 
     return unittest.TestSuite(suites)
