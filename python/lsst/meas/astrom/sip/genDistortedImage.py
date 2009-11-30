91c560bd3f9f8d2e64680b8763b4d71b01df1234
fergal <fergal@git.lsstcorp.org>
2009-11-30 17:36:47 +0000

    Added a few more ways of distorting an image.

--- python/lsst/meas/astrom/sip/genDistortedImage.py
+++ python/lsst/meas/astrom/sip/genDistortedImage.py
@@ -2,6 +2,12 @@
 import lsst.afw.image as afwImg
 import lsst.afw.detection as afwDet
 
+def noDistort(src):
+    """Do no distortion. Used for sanity checking"""
+    
+    out = afwDet.Source(src)
+    return out
+
 
 def linearXDistort(src, frac=.001):
     """Increase the xAstrom value in a Source object by frac. E.g
@@ -23,6 +29,62 @@ def linearXDistort(src, frac=.001):
     return out
 
 
+def linearYDistort(src, frac=.001):
+    """Increase the xAstrom value in a Source object by frac. E.g
+    src.xAstrom = 1000 --> 1001 if frac=.001
+    
+    Input:
+    src     A Source object
+    frac    How much to change X by
+    
+    Output:
+    A deep copy of src, with the value of xAstrom changed
+    """
+
+    out = afwDet.Source(src)
+    out.setYAstrom( out.getYAstrom()*(1+frac) )
+    return out
+
+
+def quadraticDistortX(src, frac=1e-6):
+    """Distort image by terms with power <=2
+    i.e y, y^2, x, xy, x^2
+    """
+    
+    out = afwDet.Source(src)
+    x = out.getXAstrom()
+    y = out.getYAstrom()
+    val = x**2
+    
+    out.setXAstrom(x + val*frac)
+    out.setYAstrom(y)
+    return out
+
+def quadraticDistort(src, frac=1e-6):
+    """Distort image by terms with power <=2
+    i.e y, y^2, x, xy, x^2
+    """
+    
+    out = afwDet.Source(src)
+    x = out.getXAstrom()
+    y = out.getYAstrom()
+    val =  y + 2*y**2
+    val += 3*x + 4*x*y
+    val += x**2
+    
+    out.setXAstrom(x + val*frac)
+    out.setYAstrom(y)
+    return out
+    
+def T2DistortX(src, frac=1e-6):
+    """Distort image by a 2nd order Cheby polynomial"""
+
+    out = afwDet.Source(src)
+    x = src.getXAstrom()
+    val = 2*(x**2) -1
+    out.setXAstrom( x + frac*val)
+    return out
+    
 
 def distortList(srcList, function):
     """Create a copy of srcList, and apply function to distort the 
