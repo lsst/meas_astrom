f7ce44ec26b99182fb5648f68b4cf593a2859d82
rhl <rhl@git.lsstcorp.org>
2009-12-22 20:22:49 +0000

    Merged USNOB and CFHT tests;  protect against the proper astrometry_net data being unavailable

--- tests/testGASWithUsnob.py
+++ tests/testGASWithUsnob.py
@@ -21,17 +21,21 @@ class GAS(object):
     __gas = None
     __desiredVersion = None
 
-    def __init__(self, desiredVersion):
+    def __init__(self, desiredVersion=None):
         """We use the product astrometry_net_dir to point to different index files depending on the
     problem we're trying to solve.  If any version of astrometry_net_data is setup, try to
     switch to version "desiredVersion".  There's no need to switch back as this code is running in a subprocess
     """
 
-        if desiredVersion == GAS.__desiredVersion:
+        if desiredVersion == None:
+            desiredVersion = GAS.__desiredVersion
+
+        if GAS.__desiredVersion and desiredVersion == GAS.__desiredVersion:
             return
 
         eupsObj = eups.Eups()
-        dataVersion=eupsObj.findSetupVersion("astrometry_net_data")[0]
+        dataVersion = eupsObj.findSetupVersion("astrometry_net_data")[0]
+        
         if dataVersion and dataVersion != desiredVersion:
             print >> sys.stderr, \
                   "Note: These tests require astrometry_net_data %s; Trying to set this up for you now" % \
@@ -52,7 +56,11 @@ class GAS(object):
                 an_dataDir = an_dataDir_dataDir
 
             policyFile = os.path.join(an_dataDir, "metadata.paf")
+            if not os.path.exists(policyFile):
+                raise IOError("Unable to find %s" % (policyFile))
+
             policy = pexPolicy.Policy(policyFile)
+
             indexFile = policy.get("indexFile")
 
             if os.path.exists(os.path.join(an_dataDir, indexFile)):
@@ -61,15 +69,18 @@ class GAS(object):
 
                 return
             else:
-                print >> sys.stderr, "astrometry_net_data/%s indexFiles are not available; not running %s tests"%\
-                      (desiredVersion, desiredVersion)
-        else:
-            print >> sys.stderr, "astrometry_net_data/%s is not available; not running %s tests" % \
-                  (desiredVersion, desiredVersion)
+                raise RuntimeError("astrometry_net_data/%s indexFiles are not available; " + 
+                                   "not running %s tests" % (desiredVersion, desiredVersion))
+
+        raise RuntimeError, ("astrometry_net_data/%s is not available; not running %s tests" %
+                             (desiredVersion, desiredVersion))
 
     def __del__(self):
         GAS.__gas = None
         GAS.__desiredVersion = None
+
+    def exists(self):
+        return GAS.__gas != None
 
     def __str__(self):
         return str(GAS.__gas)
@@ -77,40 +88,60 @@ class GAS(object):
     #
     # Forwarding functions
     #
+    def getDistortedWcs(self):
+        if self.exists():
+            return self.__gas.getDistortedWcs()
+        else:
+            return None
+
     def getMatchedSources(self):
-        if self.__gas:
+        if self.exists():
             return self.__gas.getMatchedSources()
         else:
             return None
 
+    def getSolvedImageScale(self):
+        if self.exists():
+            return self.__gas.getSolvedImageScale()
+        else:
+            return None
+
     def getWcs(self):
-        if self.__gas:
+        if self.exists():
             return self.__gas.getWcs()
         else:
             return None
 
     def reset(self):
-        if self.__gas:
+        if self.exists():
             self.__gas.reset()
 
     def setImageScaleArcsecPerPixel(self, plateScale):
-        if self.__gas:
+        if self.exists():
             self.__gas.setImageScaleArcsecPerPixel(plateScale)
 
     def setLogLevel(self, level):
-        if self.__gas:
+        if self.exists():
             self.__gas.setLogLevel(level)
 
+    def setMaximumImageScale(self, val):
+        if self.exists():
+            self.__gas.setMaximumImageScale(val)
+
+    def setMinimumImageScale(self, val):
+        if self.exists():
+            self.__gas.setMinimumImageScale(val)
+
     def setNumBrightObjects(self, nBright):
-        if self.__gas:
+        if self.exists():
             self.__gas.setNumBrightObjects(nBright)
         
     def setStarlist(self, starlist):
-        if self.__gas:
+        if self.exists():
             self.__gas.setStarlist(starlist)
 
     def solve(self, val=None):
-        if self.__gas:
+        if self.exists():
             if val is None:
                 return self.__gas.solve()
             else:
@@ -225,6 +256,9 @@ class WCSTestCaseNetUSNOB(unittest.TestCase):
         if verbose:
             print "testSolveGD66Wcs..."
 
+        if not self.gas.exists():
+            return
+
         crval = afwImage.PointD(80.15978319, 30.80524999)
         crpix = afwImage.PointD(890,890)
         listFile = os.path.join(eups.productDir("meas_astrom"), "tests", "gd66.xy.txt")
@@ -241,6 +275,9 @@ class WCSTestCaseNetUSNOB(unittest.TestCase):
         returned by getMatchedSources()"""
         if verbose:
             print "testSolveGD66Wcs..."
+
+        if not self.gas.exists():
+            return
 
         crval = afwImage.PointD(80.15978319, 30.80524999)
         crpix = afwImage.PointD(890,890)
@@ -280,6 +317,10 @@ class WCSTestCaseNetUSNOB(unittest.TestCase):
     def testVerifyG117(self):
         if verbose:
             print "testVerifyG117..."
+
+        if not self.gas.exists():
+            return
+
         crval = afwImage.PointD(141.063590, +35.280919)
         crpix = afwImage.PointD(446, 447)
         listFile = os.path.join(eups.productDir("meas_astrom"), "tests", "g117.xy.txt")
@@ -297,6 +338,9 @@ class WCSTestCaseNetUSNOB(unittest.TestCase):
     def testVerifyCFHTField(self):
         if verbose:
             print "testVerifyCFHTField..."
+
+        if not self.gas.exists():
+            return
         
         crval = afwImage.PointD(334.303012, -17.233988)
         
@@ -313,38 +357,213 @@ class WCSTestCaseNetUSNOB(unittest.TestCase):
         self.gas.setLogLevel(0)
 
 
-    #def testWcsSinglePixelOffset(self):
-        #mastrom = eups.productDir("meas_astrom")
-        #imageFilename = "gd66.fits"
-        #starlist = os.path.join(mastrom, "tests", "gd66.xy.txt")
-#
-#
-        #filename = os.path.join(mastrom, "tests", "gd66.fits")
-#
-        #starlist = loadXYFromFile(starlist)
-#
-        ##Get Wcs from image header
-        #exposure = afwImg.ExposureF(filename)
-        #origWcs = exposure.getWcs()
-#
-        ##Get Wcs from astrometry.net
-        #gasWcs = self.gas.solveWcs(starlist, origWcs)
-#
-        ##Pick an radec. The xy values corresponding to this radec should
-        ##differ by sqrt(2) between the two wcs'. Also, the values for
-        ##gasWcs should be larger in both axes
-        #radec = afwImage.PointD(80.139800, +30.7864306)
-        #origPix = origWcs.raDecToXY(radec)
-        #gasPix = gasWcs.raDecToXY(radec)
-#
-#
-        #self.assertTrue(origPix.getX() <= gasPix.getX(), "GAS Wcs moved in wrong direction in X")
-        #self.assertTrue(origPix.getY() <= gasPix.getY(), "GAS Wcs moved in wrong direction in Y")
-#
-        #ds = origPix-gasPix
-        #ds = math.hypot(ds.getX(), ds.getY() )
-        #self.assertAlmostEqual(ds, Math.sqrt(2), 1, "Distance moved not 1 pixel")    
+    if False:
+        def testWcsSinglePixelOffset(self):
+            if not self.gas.exists():
+                return
+
+            mastrom = eups.productDir("meas_astrom")
+            imageFilename = "gd66.fits"
+            starlist = os.path.join(mastrom, "tests", "gd66.xy.txt")
+
+
+            filename = os.path.join(mastrom, "tests", "gd66.fits")
+
+            starlist = loadXYFromFile(starlist)
+
+            #Get Wcs from image header
+            exposure = afwImg.ExposureF(filename)
+            origWcs = exposure.getWcs()
+
+            #Get Wcs from astrometry.net
+            gasWcs = self.gas.solveWcs(starlist, origWcs)
+
+            #Pick an radec. The xy values corresponding to this radec should
+            #differ by sqrt(2) between the two wcs'. Also, the values for
+            #gasWcs should be larger in both axes
+            radec = afwImage.PointD(80.139800, +30.7864306)
+            origPix = origWcs.raDecToXY(radec)
+            gasPix = gasWcs.raDecToXY(radec)
+
+
+            self.assertTrue(origPix.getX() <= gasPix.getX(), "GAS Wcs moved in wrong direction in X")
+            self.assertTrue(origPix.getY() <= gasPix.getY(), "GAS Wcs moved in wrong direction in Y")
+
+            ds = origPix-gasPix
+            ds = math.hypot(ds.getX(), ds.getY() )
+            self.assertAlmostEqual(ds, Math.sqrt(2), 1, "Distance moved not 1 pixel")    
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+class SmallSolveGASTestCFHT(unittest.TestCase):
+    """A test case for WCS from astrometry.net using a small set of index files"""
+
+    def setUp(self):
+        self.gas = GAS("cfhttemplate")
+
+    def tearDown(self):
+        self.gas.reset()
+
+    def solve(self, imgListFile, raDec, nBright=50, expectPass=True):
+        starlist = loadXYFromFile(imgListFile)
+        self.gas.setStarlist(starlist)
+        self.gas.setNumBrightObjects(50)
     
+        flag = self.gas.solve(raDec)
+        if expectPass:
+            self.assertTrue(flag, "No solution found")
+            wcs = self.gas.getWcs()
+            result = wcs.getOriginRaDec()
+            scale= self.gas.getSolvedImageScale()
+            if verbose:
+                print "%.6f %.6f %.3f" %(result.getX(), result.getY(), scale)
+
+        else:
+            self.assertFalse(flag, "Solution found, but none expected")
+    
+    def testGD66Fail(self):
+        """A field not covered by the CFHT indices"""
+
+        if not self.gas.exists():
+            return
+
+        crval = afwImage.PointD(80.15978319,30.80524999)
+
+        #Set starlist    
+        starlist = os.path.join(eups.productDir("meas_astrom"), "tests", "gd66.xy.txt")
+        self.gas.reset()
+        self.gas.setMinimumImageScale(.5)
+        self.gas.setMaximumImageScale(2)
+        self.solve(starlist, crval, expectPass=False)
+        #
+        
+        
+    def testCFHTa(self):                
+        """Testing a field that should pass with different image scales"""
+        if verbose:
+            print "testCFHTa"
+
+        if not self.gas.exists():
+            return
+
+        crval = afwImage.PointD(334.303012, -17.233988)
+        #Set starlist    
+        starlist = os.path.join(eups.productDir("meas_astrom"), "tests", "cfht.xy.txt")
+
+        self.gas.reset()
+        self.gas.setMinimumImageScale(.1)
+        self.gas.setMaximumImageScale(.2)
+        self.gas.setLogLevel(verbose)
+        self.solve(starlist, crval)
+        self.gas.setLogLevel(0)
+
+        #
+    def testCFHTb(self):                
+        """Different starting point"""
+        if verbose:
+            print "testCFHTb"
+            
+        if not self.gas.exists():
+            return
+
+        crval = afwImage.PointD(334.303215, -17.329315)
+        #Set starlist    
+        starlist = os.path.join(eups.productDir("meas_astrom"), "tests", "cfht.xy.txt")
+
+        self.gas.reset()
+        self.gas.setMinimumImageScale(.1)
+        self.gas.setMaximumImageScale(.5)
+        if verbose:
+            self.gas.setLogLevel(3)
+        self.solve(starlist, crval)    
+        self.gas.setLogLevel(0)
+
+
+    def testCFHTc(self):                
+        """Different img scales"""
+        if verbose:
+            print "testCFHTc"
+
+        if not self.gas.exists():
+            return
+
+        crval = afwImage.PointD(334.303215, -17.329315)
+        #Set starlist    
+        starlist = os.path.join(eups.productDir("meas_astrom"), "tests", "cfht.xy.txt")
+
+        self.gas.reset()
+        self.gas.setMinimumImageScale(.1)
+        self.gas.setMaximumImageScale(1.5)
+        if verbose:
+            self.gas.setLogLevel(3)
+        self.solve(starlist, crval)    
+        self.gas.setLogLevel(0)
+
+
+    def testCFHTd(self):                
+        """Different img scales"""
+
+        if verbose:
+            print "testCFHTc"
+
+        if not self.gas.exists():
+            return
+
+        crval = afwImage.PointD(334.303215, -17.329315)
+        #Set starlist    
+        starlist = os.path.join(eups.productDir("meas_astrom"), "tests", "cfht.xy.txt")
+
+        self.gas.reset()
+        self.gas.setMinimumImageScale(.15)
+        self.gas.setMaximumImageScale(.25)
+        if verbose:
+            self.gas.setLogLevel(3)
+        self.solve(starlist, crval)    
+        self.gas.setLogLevel(0)
+
+
+    def testCFHTe(self):                
+        """Different img scales"""
+        if verbose:
+            print "testCFHTe"
+
+        if not self.gas.exists():
+            return
+
+        crval = afwImage.PointD(334.303215, -17.329315)
+        #Set starlist    
+        starlist = os.path.join(eups.productDir("meas_astrom"), "tests", "cfht.xy.txt")
+
+        self.gas.reset()
+        self.gas.setImageScaleArcsecPerPixel(.183)
+        if verbose:
+            self.gas.setLogLevel(3)
+        self.solve(starlist, crval)    
+        self.gas.setLogLevel(0)
+        wcs = self.gas.getWcs()
+
+    def testDistortedWcs(self):
+        """Is a distorted Wcs returned"""
+        if verbose:
+            print "DistortedWcs"
+        """Different img scales"""
+        crval = afwImage.PointD(334.303215, -17.329315)
+        #Set starlist    
+        starlist = os.path.join(eups.productDir("meas_astrom"), "tests", "cfht.xy.txt")
+
+        self.gas.reset()
+        self.gas.setImageScaleArcsecPerPixel(.183)
+        if verbose:
+            self.gas.setLogLevel(3)
+        self.solve(starlist, crval)    
+        wcs = self.gas.getDistortedWcs()
+        self.gas.setLogLevel(0)
+
+
+    def testSolveWcs(self):
+        """Can't be tested until I figure out the LSST way of interfacing Eigen Matrices"""
+        pass
+
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 
@@ -353,8 +572,8 @@ def suite():
     utilsTests.init()
 
     suites = []
-    #gas = GAS("usnob")
     suites += unittest.makeSuite(WCSTestCaseNetUSNOB)
+    suites += unittest.makeSuite(SmallSolveGASTestCFHT)
     suites += unittest.makeSuite(utilsTests.MemoryTestCase)
 
     return unittest.TestSuite(suites)
