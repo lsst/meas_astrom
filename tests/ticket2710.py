f4fc7d831ccd11a80701a5cfd2f33fa6c4f37a83
Dustin Lang <dstndstn@gmail.com>
2013-03-01 14:41:56 -0500

    tweak the plots

--- tests/ticket2710.py
+++ tests/ticket2710.py
@@ -48,7 +48,6 @@ def main():
         
     x0,y0 = 335750, 223750
     W,H = 8500, 12500
-
     imargs = dict(imageSize=(W,H), filterName='i', x0=x0, y0=y0)
     
     # Read original WCS
@@ -73,16 +72,17 @@ def main():
 
     refs = ast.getReferenceSourcesForWcs(wcs0, **imargs)
     print 'Got', len(refs), 'reference objects for initial WCS'
-    
-    solve = ast.determineWcs2(srcs, **imargs)
-    wcs1 = solve.getSipWcs()
-    #print 'wcs1:', wcs1.getFitsMetadata().toString()
-    wcs1tan = solve.getTanWcs()
 
-    wcs2 = ast.getSipWcsFromWcs(wcs0, wcs1tan, (W,H), x0=x0, y0=y0)
-    
+
+    conf2 = measAstrom.MeasAstromConfig(sipOrder=4, calculateSip=False)
+    ast2 = measAstrom.Astrometry(conf2, andConfig, logLevel=pexLog.Log.DEBUG)
+    solve = ast2.determineWcs2(srcs, **imargs)
+    tanwcs = solve.getTanWcs()
+
+    wcs2 = ast.getSipWcsFromWcs(wcs0, tanwcs, (W,H), x0=x0, y0=y0)
+
+
     rx0,ry0 = [],[]
-    rx1,ry1 = [],[]
     rx2,ry2 = [],[]
     rx3,ry3 = [],[]
 
@@ -91,11 +91,7 @@ def main():
         rx0.append(xy[0])
         ry0.append(xy[1])
 
-        xy = wcs1.skyToPixel(src.getCoord())
-        rx1.append(xy[0])
-        ry1.append(xy[1])
-
-        xy = wcs1tan.skyToPixel(src.getCoord())
+        xy = tanwcs.skyToPixel(src.getCoord())
         rx2.append(xy[0])
         ry2.append(xy[1])
 
@@ -105,70 +101,71 @@ def main():
         
     rx0 = np.array(rx0)
     ry0 = np.array(ry0)
-    rx1 = np.array(rx1)
-    ry1 = np.array(ry1)
     rx2 = np.array(rx2)
     ry2 = np.array(ry2)
     rx3 = np.array(rx3)
     ry3 = np.array(ry3)
 
+
     from astrometry.libkd.spherematch import match
     from astrometry.util.plotutils import plothist,PlotSequence
 
     ps = PlotSequence('t2710')
+
+    R = 2.
     
-    plt.clf()
-    plt.plot(x, y, 'o', mec='r', mfc='none')
-    plt.plot(rx0, ry0, 'bx')
-    plt.plot(rx1, ry1, 'g+')
-    plt.plot(rx2, ry2, 'mx')
-    plt.plot(rx3, ry3, 'r+')
-    ps.savefig()
-
-    plt.axis([x0, x0+500, y0, y0+500])
-    ps.savefig()
-
-    II,d = match(np.vstack((x,y)).T, np.vstack((rx0,ry0)).T, 10.)
+    II,d = match(np.vstack((x,y)).T, np.vstack((rx0,ry0)).T, R)
     I = II[:,0]
     J = II[:,1]
 
+    pa = dict(range=((-R,R),(-R,R)))
+    
     plt.clf()
-    plothist(x[I]-rx0[J], y[I]-ry0[J], 200)
+    plothist(x[I]-rx0[J], y[I]-ry0[J], 200, **pa)
     plt.title('Source positions - Reference positions (initial WCS)')
+    plt.xlabel('delta-X (pixels)')
+    plt.ylabel('delta-Y (pixels)')
     ps.savefig()
 
-    II,d = match(np.vstack((x,y)).T, np.vstack((rx1,ry1)).T, 10.)
+
+    II,d = match(np.vstack((x,y)).T, np.vstack((rx2,ry2)).T, R)
     I = II[:,0]
     J = II[:,1]
     
     plt.clf()
-    plothist(x[I]-rx1[J], y[I]-ry1[J], 200)
-    plt.title('Source positions - Reference positions (SIP WCS)')
-    ps.savefig()
-
-    II,d = match(np.vstack((x,y)).T, np.vstack((rx2,ry2)).T, 10.)
-    I = II[:,0]
-    J = II[:,1]
-    
-    plt.clf()
-    plothist(x[I]-rx2[J], y[I]-ry2[J], 200)
+    plothist(x[I]-rx2[J], y[I]-ry2[J], 200, **pa)
     plt.title('Source positions - Reference positions (TAN WCS)')
+    plt.xlabel('delta-X (pixels)')
+    plt.ylabel('delta-Y (pixels)')
     ps.savefig()
 
-    II,d = match(np.vstack((x,y)).T, np.vstack((rx3,ry3)).T, 10.)
+    II,d = match(np.vstack((x,y)).T, np.vstack((rx3,ry3)).T, R)
     I = II[:,0]
     J = II[:,1]
     plt.clf()
-    plothist(x[I]-rx3[J], y[I]-ry3[J], 200)
+    plothist(x[I]-rx3[J], y[I]-ry3[J], 200, **pa)
     plt.title('Source positions - Reference positions (SIP WCS #2)')
+    plt.xlabel('delta-X (pixels)')
+    plt.ylabel('delta-Y (pixels)')
     ps.savefig()
+
+    II,d = match(np.vstack((rx0,ry0)).T, np.vstack((rx3,ry3)).T, R)
+    I = II[:,0]
+    J = II[:,1]
+    plt.clf()
+    plothist(rx0[I]-rx3[J], ry0[I]-ry3[J], 200, **pa)
+    plt.title('Reference positions (Original WCS) - Reference positions (SIP WCS #2)')
+    plt.xlabel('delta-X (pixels)')
+    plt.ylabel('delta-Y (pixels)')
+    ps.savefig()
+
     
     matches = solve.getTanMatches()
     msx,msy = [],[]
     mrx,mry = [],[]
     for m in matches:
         ref,src = m.first, m.second
-        xy = wcs1tan.skyToPixel(ref.getCoord())
+        xy = tanwcs.skyToPixel(ref.getCoord())
         mrx.append(xy[0])
         mry.append(xy[1])
         msx.append(src.getX())
@@ -185,12 +182,18 @@ def main():
     ps.savefig()
 
 
+
+    solve = ast.determineWcs2(srcs, **imargs)
+    wcs1 = solve.getSipWcs()
+    #print 'wcs1:', wcs1.getFitsMetadata().toString()
+
+
     matches = solve.getSipMatches()
     msx,msy = [],[]
     mrx,mry = [],[]
     for m in matches:
         ref,src = m.first, m.second
-        xy = wcs1tan.skyToPixel(ref.getCoord())
+        xy = tanwcs.skyToPixel(ref.getCoord())
         mrx.append(xy[0])
         mry.append(xy[1])
         msx.append(src.getX())
@@ -204,6 +207,38 @@ def main():
     plt.title('SIP matches')
     ps.savefig()
 
+
+    
+    rx1,ry1 = [],[]
+    for src in refs:
+        xy = wcs1.skyToPixel(src.getCoord())
+        rx1.append(xy[0])
+        ry1.append(xy[1])
+    rx1 = np.array(rx1)
+    ry1 = np.array(ry1)
+
+    plt.clf()
+    plt.plot(x, y, 'o', mec='r', mfc='none')
+    plt.plot(rx0, ry0, 'bx')
+    plt.plot(rx1, ry1, 'g+')
+    plt.plot(rx2, ry2, 'mx')
+    plt.plot(rx3, ry3, 'r+')
+    ps.savefig()
+
+    plt.axis([x0, x0+500, y0, y0+500])
+    ps.savefig()
+
+    II,d = match(np.vstack((x,y)).T, np.vstack((rx1,ry1)).T, R)
+    I = II[:,0]
+    J = II[:,1]
+    
+    plt.clf()
+    plothist(x[I]-rx1[J], y[I]-ry1[J], 200, **pa)
+    plt.title('Source positions - Reference positions (SIP WCS)')
+    plt.xlabel('delta-X (pixels)')
+    plt.ylabel('delta-Y (pixels)')
+    ps.savefig()
+    
     
     
 
