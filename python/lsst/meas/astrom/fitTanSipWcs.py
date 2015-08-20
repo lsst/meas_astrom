e18a2264bfbe36946520e992d53a50b061cffdcf
Paul Price <price@astro.princeton.edu>
2015-08-19 22:01:19 -0700

    FitTanSipWcsTask: add rejection and visualisation
    We can get some rogue matches which should be rejected as part
    of the Wcs fitting (but that's not done in CreateWcsWithSip).
    To help visualise the rejection, added some plots of the
    residuals.

--- python/lsst/meas/astrom/fitTanSipWcs.py
+++ python/lsst/meas/astrom/fitTanSipWcs.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
+import numpy
 import lsst.afw.image as afwImage
 import lsst.afw.coord as afwCoord
 import lsst.afw.geom as afwGeom
@@ -24,6 +25,18 @@ class FitTanSipWcsConfig(pexConfig.Config):
         dtype = int,
         default = 3,
         min = 1,
+    )
+    numRejIter = pexConfig.RangeField(
+        doc = "number of rejection iterations",
+        dtype = int,
+        default = 1,
+        min = 0,
+    )
+    rejSigma = pexConfig.RangeField(
+        doc = "Number of standard deviations for clipping level",
+        dtype = float,
+        default = 3.0,
+        min = 0.0,
     )
     maxScatterArcsec = pexConfig.RangeField(
         doc = "maximum median scatter of a WCS fit beyond which the fit fails (arcsec); " +
@@ -117,10 +130,27 @@ class FitTanSipWcsTask(pipeBase.Task):
         """
         if bbox is None:
             bbox = afwGeom.Box2I()
+
+        import lsstDebug
+        debug = lsstDebug.Info(__name__)
+
         wcs = self.initialWcs(matches, initWcs)
-        for i in range(self.config.numIter):
-            sipObject = makeCreateWcsWithSip(matches, wcs, self.config.order, bbox)
+        rejected = numpy.zeros(len(matches), dtype=bool)
+        for rej in range(self.config.numRejIter):
+            sipObject = self._fitWcs([mm for i, mm in enumerate(matches) if not rejected[i]], wcs)
             wcs = sipObject.getNewWcs()
+            rejected = self.rejectMatches(matches, wcs, rejected)
+            if rejected.sum() == len(rejected):
+                raise RuntimeError("All matches rejected in iteration %d" % (rej + 1,))
+            if debug.plot:
+                print("Plotting fit after rejection iteration %d/%d" % (rej + 1, self.config.numRejIter))
+                self.plotFit(matches, wcs, rejected)
+        # Final fit after rejection
+        sipObject = self._fitWcs([mm for i, mm in enumerate(matches) if not rejected[i]], wcs)
+        wcs = sipObject.getNewWcs()
+        if debug.plot:
+            print("Plotting final fit")
+            self.plotFit(matches, wcs, rejected)
 
         if refCat is not None:
             self.log.info("Updating centroids in refCat")
@@ -171,6 +201,26 @@ class FitTanSipWcsTask(pipeBase.Task):
                               afwGeom.Point2D(crpix), wcs.getCDMatrix())
         return newWcs
 
+    def _fitWcs(self, matches, wcs):
+        """Fit a Wcs based on the matches and a guess Wcs"""
+        for i in range(self.config.numIter):
+            sipObject = makeCreateWcsWithSip(matches, wcs, self.config.order)
+            wcs = sipObject.getNewWcs()
+        return sipObject
+
+    def rejectMatches(self, matches, wcs, rejected):
+        """Flag deviant matches
+
+        We return a boolean numpy array indicating whether the corresponding
+        match should be rejected.  The previous list of rejections is used
+        so we can calculate uncontaminated statistics.
+        """
+        fit = [wcs.skyToPixel(m.first.getCoord()) for m in matches]
+        dx = numpy.array([ff.getX() - mm.second.getCentroid().getX() for ff, mm in zip(fit, matches)])
+        dy = numpy.array([ff.getY() - mm.second.getCentroid().getY() for ff, mm in zip(fit, matches)])
+        good = numpy.logical_not(rejected)
+        return (dx > self.config.rejSigma*dx[good].std()) | (dy > self.config.rejSigma*dy[good].std())
+
     @staticmethod
     def updateRefCentroids(wcs, refList):
         """Update centroids in a collection of reference objects, given a WCS
@@ -187,10 +237,55 @@ class FitTanSipWcsTask(pipeBase.Task):
     def updateSourceCoords(wcs, sourceList):
         """Update coords in a collection of sources, given a WCS
         """
-
         if len(sourceList) < 1:
             return
         schema = sourceList[1].schema
         srcCoordKey = afwTable.CoordKey(schema["coord"])
         for src in sourceList:
             src.set(srcCoordKey, wcs.pixelToSky(src.getCentroid()))
+
+    def plotFit(self, matches, wcs, rejected):
+        """Plot the fit
+
+        We create four plots, for all combinations of (dx, dy) against
+        (x, y).  Good points are black, while rejected points are red.
+        """
+        import matplotlib.pyplot as plt
+
+        fit = [wcs.skyToPixel(m.first.getCoord()) for m in matches]
+        x1 = numpy.array([ff.getX() for ff in fit])
+        y1 = numpy.array([ff.getY() for ff in fit])
+        x2 = numpy.array([m.second.getCentroid().getX() for m in matches])
+        y2 = numpy.array([m.second.getCentroid().getY() for m in matches])
+
+        dx = x1 - x2
+        dy = y1 - y2
+
+        good = numpy.logical_not(rejected)
+
+        figure = plt.figure()
+        axes = figure.add_subplot(2, 2, 1)
+        axes.plot(x2[good], dx[good], 'ko')
+        axes.plot(x2[rejected], dx[rejected], 'ro')
+        axes.set_xlabel("x")
+        axes.set_ylabel("dx")
+
+        axes = figure.add_subplot(2, 2, 2)
+        axes.plot(x2[good], dy[good], 'ko')
+        axes.plot(x2[rejected], dy[rejected], 'ro')
+        axes.set_xlabel("x")
+        axes.set_ylabel("dy")
+
+        axes = figure.add_subplot(2, 2, 3)
+        axes.plot(y2[good], dx[good], 'ko')
+        axes.plot(y2[rejected], dx[rejected], 'ro')
+        axes.set_xlabel("y")
+        axes.set_ylabel("dx")
+
+        axes = figure.add_subplot(2, 2, 4)
+        axes.plot(y2[good], dy[good], 'ko')
+        axes.plot(y2[rejected], dy[rejected], 'ro')
+        axes.set_xlabel("y")
+        axes.set_ylabel("dy")
+
+        plt.show()
