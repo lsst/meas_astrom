c5cddc11ea16d970409a210e5515446ce384419a
Russell Owen <rowen@uw.edu>
2015-09-17 15:44:19 -0700

    Change assertTrue to assertLess, etc. in tests
    Many unit tests were using assertTrue when testing
    relative values; this is harder to reada and results in less
    useful error messages than assertLess, assertGreater, etc.
    so I changed all instances that I found.

--- tests/SourceMatchJoin.py
+++ tests/SourceMatchJoin.py
@@ -76,7 +76,7 @@ class matchlistTestCase(unittest.TestCase):
         normalized.table.setMetadata(matchmeta)
 
         matches2 = self.astrom.joinMatchListWithCatalog(normalized, self.srcSet)
-        self.assertTrue(len(matches2) > 0)
+        self.assertGreater(len(matches2), 0)
         ref = matches2[0][0]
 
         names = ref.getSchema().getNames()
