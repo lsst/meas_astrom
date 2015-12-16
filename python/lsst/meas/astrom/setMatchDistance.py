38e9b763fabdcea84966d94da85d375edf82560d
Lauren MacArthur <lauren@astro.princeton.edu>
2015-12-16 12:07:44 -0500

    Move joinMatchListWithCatalog to setMatchDistance.py
    This function used to reside in anetBasicAstrometry.py, but it is
    of use to any AstrometryTask so we move it to a seperate file
    (currently called setMatchDistance.py, but may rename to matchUtils.py
    for inclusion of any functions providing general utilities for match
    catalogs).  In addition to the lists of packed matches and sources,
    the caller may also provide their initialized AstrometryTask.  If
    None is provided, meas.astrom.astrometry.AstrometryTask will be
    initialized.

--- python/lsst/meas/astrom/setMatchDistance.py
+++ python/lsst/meas/astrom/setMatchDistance.py
@@ -20,10 +20,11 @@ from __future__ import absolute_import, division, print_function
 # the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
-
+import lsst.afw.coord as afwCoord
+import lsst.afw.geom as afwGeom
 import lsst.afw.table as afwTable
 
-__all__ = ["setMatchDistance"]
+__all__ = ["setMatchDistance", "joinMatchListWithCatalog"]
 
 def setMatchDistance(matches):
     """Set the distance field of the matches in a match list to the distance in radians on the sky
@@ -43,3 +44,50 @@ def setMatchDistance(matches):
         sourceCoord = match.first.get(sourceCoordKey)
         refObjCoord = match.second.get(refObjCoordKey)
         match.distance = refObjCoord.angularSeparation(sourceCoord).asRadians()
+
+
+def joinMatchListWithCatalog(packedMatches, sourceCat, astrom=None):
+    """
+    This function is required to reconstitute a ReferenceMatchVector after being
+    unpersisted.  The persisted form of a ReferenceMatchVector is the
+    normalized Catalog of IDs produced by afw.table.packMatches(), with the result of
+    InitialAstrometry.getMatchMetadata() in the associated tables\' metadata.
+
+    The "live" form of a matchlist has links to
+    the real record objects that are matched; it is "denormalized".
+    This function takes a normalized match catalog, along with the catalog of
+    sources to which the match catalog refers.  It fetches the reference
+    sources that are within range, and then denormalizes the matches
+    -- sets the "matches[*].first" and "matches[*].second" entries
+    to point to the sources in the "sourceCat" argument, and to the
+    reference sources fetched from the astrometry_net_data files.
+
+    @param[in] packedMatches  Unpersisted match list (an lsst.afw.table.BaseCatalog).
+                              packedMatches.table.getMetadata() must contain the
+                              values from InitialAstrometry.getMatchMetadata()
+    @param[in,out] sourceCat  Source catalog used for the 'second' side of the matches
+                              (an lsst.afw.table.SourceCatalog).  As a side effect,
+                              the catalog will be sorted by ID.
+    @param[in] astrom         Initialized Astrometry task (must have an refObjLoader.loadSkyCircle()
+                              function).  If None, meas.astrom.AstrometryTask will be initialized.
+
+    @return An lsst.afw.table.ReferenceMatchVector of denormalized matches.
+    """
+    if astrom is None:
+        from .astrometry import AstrometryTask, AstrometryConfig
+        astrom = AstrometryTask(AstrometryConfig)
+
+    matchmeta = packedMatches.table.getMetadata()
+    version = matchmeta.getInt('SMATCHV')
+    if version != 1:
+        raise ValueError('SourceMatchVector version number is %i, not 1.' % version)
+    filterName = matchmeta.getString('FILTER').strip()
+    ctrCoord = afwCoord.IcrsCoord(
+        matchmeta.getDouble('RA') * afwGeom.degrees,
+        matchmeta.getDouble('DEC') * afwGeom.degrees,
+        )
+    rad = matchmeta.getDouble('RADIUS') * afwGeom.degrees
+    refCat = astrom.refObjLoader.loadSkyCircle(ctrCoord, rad, filterName).refCat
+    refCat.sort()
+    sourceCat.sort()
+    return afwTable.unpackMatches(packedMatches, refCat, sourceCat)
