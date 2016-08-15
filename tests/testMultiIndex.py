41995abc65f1ac009785205c0fa68e3955b0b75b
Russell Owen <rowen@uw.edu>
2016-08-15 16:08:25 -0700

    Modernize the way testMultiIndex is run

--- tests/testMultiIndex.py
+++ tests/testMultiIndex.py
@@ -173,22 +173,14 @@ class MultiIndexTest(unittest.TestCase):
         print 'FD3:', fd3
 
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+class MemoryTester(lsst.utils.tests.MemoryTestCase):
+    pass
 
-def suite():
-    """Returns a suite containing all the test cases in this module."""
+
+def setup_module(module):
     lsst.utils.tests.init()
 
-    suites = []
-    suites += unittest.makeSuite(MultiIndexTest)
-    suites += unittest.makeSuite(lsst.utils.tests.MemoryTestCase)
-
-    return unittest.TestSuite(suites)
-
-
-def run(exit=False):
-    """Run the tests"""
-    lsst.utils.tests.run(suite(), exit)
 
 if __name__ == "__main__":
-    run(True)
+    lsst.utils.tests.init()
+    unittest.main()
