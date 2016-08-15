3480f9284fc906e9c9554e594589eea161bd0504
Russell Owen <rowen@uw.edu>
2016-08-15 14:00:53 -0700

    Modernize the way openFiles.py is called

--- tests/openFiles.py
+++ tests/openFiles.py
@@ -118,20 +118,14 @@ class OpenFilesTest(unittest.TestCase):
         self.runUseKnownWcs(wcs)
 
 
-def suite():
-    """Returns a suite containing all the test cases in this module."""
+class MemoryTester(lsst.utils.tests.MemoryTestCase):
+    pass
+
+
+def setup_module(module):
     lsst.utils.tests.init()
 
-    suites = []
-    suites += unittest.makeSuite(OpenFilesTest)
-    suites += unittest.makeSuite(lsst.utils.tests.MemoryTestCase)
-
-    return unittest.TestSuite(suites)
-
-
-def run(exit=False):
-    """Run the tests"""
-    lsst.utils.tests.run(suite(), exit)
 
 if __name__ == "__main__":
-    run(True)
+    lsst.utils.tests.init()
+    unittest.main()
