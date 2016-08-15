9207f0c3873c0558674f39147a8f3c208bc3e222
Russell Owen <rowen@uw.edu>
2016-08-15 14:13:08 -0700

    Modernize the way testAstrometryTask.py is run

--- tests/testAstrometryTask.py
+++ tests/testAstrometryTask.py
@@ -166,23 +166,15 @@ class TestAstrometricSolver(lsst.utils.tests.TestCase):
             src.set(sourceFluxSigmaKey, refObj.get(refFluxRKey)/100)
         return sourceCat
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+class MemoryTester(lsst.utils.tests.MemoryTestCase):
+    pass
 
 
-def suite():
-    """Returns a suite containing all the test cases in this module."""
+def setup_module(module):
     lsst.utils.tests.init()
 
-    suites = []
-    suites += unittest.makeSuite(TestAstrometricSolver)
-    suites += unittest.makeSuite(lsst.utils.tests.MemoryTestCase)
-
-    return unittest.TestSuite(suites)
-
-
-def run(exit=False):
-    """Run the tests"""
-    lsst.utils.tests.run(suite(), exit)
 
 if __name__ == "__main__":
-    run(True)
+    lsst.utils.tests.init()
+    unittest.main()
