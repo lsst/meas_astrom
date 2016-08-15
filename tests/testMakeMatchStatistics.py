4828225285945209446c85862e8e0cd3e89fad8f
Russell Owen <rowen@uw.edu>
2016-08-15 15:59:40 -0700

    Modernize the way testMakeMatchStatistics is run

--- tests/testMakeMatchStatistics.py
+++ tests/testMakeMatchStatistics.py
@@ -147,22 +147,14 @@ class TestAstrometricSolver(lsst.utils.tests.TestCase):
             self.assertAlmostEqual(distStats.getValue(item), directStats.getValue(item))
 
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+class MemoryTester(lsst.utils.tests.MemoryTestCase):
+    pass
 
-def suite():
-    """Returns a suite containing all the test cases in this module."""
+
+def setup_module(module):
     lsst.utils.tests.init()
 
-    suites = []
-    suites += unittest.makeSuite(TestAstrometricSolver)
-    suites += unittest.makeSuite(lsst.utils.tests.MemoryTestCase)
-
-    return unittest.TestSuite(suites)
-
-
-def run(exit=False):
-    """Run the tests"""
-    lsst.utils.tests.run(suite(), exit)
 
 if __name__ == "__main__":
-    run(True)
+    lsst.utils.tests.init()
+    unittest.main()
