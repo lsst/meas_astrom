d3dc3afca73a101d2caf686dd8b997e159b445bc
Russell Owen <rowen@uw.edu>
2016-08-15 15:43:19 -0700

    Modernize the way testJoinMatchList... is run

--- tests/testJoinMatchListWithCatalog.py
+++ tests/testJoinMatchListWithCatalog.py
@@ -111,21 +111,15 @@ class joinMatchListWithCatalogTestCase(unittest.TestCase):
             self.assertIn("%s_flux" % (b,), names)
             self.assertIn("%s_fluxSigma" % (b,), names)
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+class MemoryTester(lsst.utils.tests.MemoryTestCase):
+    pass
 
 
-def suite():
-    """Returns a suite containing all the test cases in this module."""
+def setup_module(module):
     lsst.utils.tests.init()
-    suites = []
-    suites += unittest.makeSuite(joinMatchListWithCatalogTestCase)
-    suites += unittest.makeSuite(lsst.utils.tests.MemoryTestCase)
-    return unittest.TestSuite(suites)
 
-
-def run(shouldExit=False):
-    """Run the utilsTests"""
-    lsst.utils.tests.run(suite(), shouldExit)
 
 if __name__ == "__main__":
-    run(True)
+    lsst.utils.tests.init()
+    unittest.main()
