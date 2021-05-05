385d9ff9b976cdb73b6a9ff87a86f623c6174d62
Jonathan Sick <jsick@lsst.org>
2021-05-05 13:38:38 -0400

    Update Sphinx config for Documenteer 0.6

--- doc/conf.py
+++ doc/conf.py
@@ -1,13 +1,13 @@
 """Sphinx configuration file for an LSST stack package.
-
-This configuration only affects single-package Sphinx documenation builds.
+This configuration only affects single-package Sphinx documentation builds.
+For more information, see:
+https://developer.lsst.io/stack/building-single-package-docs.html
 """
 
-from documenteer.sphinxconfig.stackconf import build_package_configs
-import lsst.meas.astrom
+from documenteer.conf.pipelinespkg import *
 
 
-_g = globals()
-_g.update(build_package_configs(
-    project_name='meas_astrom',
-    version=lsst.meas.astrom.version.__version__))
+project = "meas_astrom"
+html_theme_options["logotext"] = project
+html_title = project
+html_short_title = project
