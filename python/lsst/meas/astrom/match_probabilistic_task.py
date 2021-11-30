a12e5d6d16b4ed7fdc6fbf162c3650db2f14d92d
taranu <dtaranu@astro.princeton.edu>
2021-11-30 15:14:38 -0600

    Add MatcherProbabilistic and MatchProbabilisticTask

---
+++ python/lsst/meas/astrom/match_probabilistic_task.py
@@ -0,0 +1,174 @@
+# This file is part of meas_astrom.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import lsst.afw.geom as afwGeom
+import lsst.geom as geom
+import lsst.pipe.base as pipeBase
+import lsst.utils as utils
+from .matcher_probabilistic import MatchProbabilisticConfig, MatcherProbabilistic
+
+import logging
+import numpy as np
+import pandas as pd
+from typing import Dict, Set, Tuple
+
+__all__ = ['MatchProbabilisticTask']
+
+
+class MatchProbabilisticTask(pipeBase.Task):
+    """Run MatchProbabilistic on a reference and target catalog covering the same tract.
+    """
+    ConfigClass = MatchProbabilisticConfig
+    _DefaultName = "matchProbabilistic"
+
+    @property
+    def columns_in_ref(self) -> Set[str]:
+        return self.config.columns_in_ref()
+
+    @property
+    def columns_in_target(self) -> Set[str]:
+        return self.config.columns_in_target()
+
+    def match(
+        self,
+        catalog_ref: pd.DataFrame,
+        catalog_target: pd.DataFrame,
+        select_ref: np.array = None,
+        select_target: np.array = None,
+        wcs: afwGeom.SkyWcs = None,
+        logger: logging.Logger = None,
+        logging_n_rows: int = None,
+    ) -> Tuple[pd.DataFrame, pd.DataFrame, Dict[int, str]]:
+        """Match sources in a reference tract catalog with a target catalog.
+
+        Parameters
+        ----------
+        catalog_ref : `pandas.DataFrame`
+            A reference catalog to match objects/sources from.
+        catalog_target : `pandas.DataFrame`
+            A target catalog to match reference objects/sources to.
+        select_ref : `numpy.array`
+            A boolean array of the same length as `catalog_ref` selecting the sources that can be matched.
+        select_target : `numpy.array`
+            A boolean array of the same length as `catalog_target` selecting the sources that can be matched.
+        wcs : `lsst.afw.image.SkyWcs`
+            A coordinate system to convert catalog positions to sky coordinates. Only used if
+            `self.config.coords_ref_to_convert` is set.
+        logger : `logging.Logger`
+            A Logger for logging.
+        logging_n_rows : `int`
+            Number of matches to make before outputting incremental log message.
+
+        Returns
+        -------
+        catalog_out_ref : `pandas.DataFrame`
+            Reference matched catalog with indices of target matches.
+        catalog_out_target : `pandas.DataFrame`
+            Reference matched catalog with indices of target matches.
+        """
+        if logger is None:
+            logger = self.log
+
+        config = self.config
+
+        if config.column_order is None:
+            flux_tot = np.nansum(catalog_ref.loc[:, config.columns_ref_flux].values, axis=1)
+            catalog_ref['flux_total'] = flux_tot
+            if config.mag_brightest_ref != -np.inf or config.mag_faintest_ref != np.inf:
+                mag_tot = -2.5 * np.log10(flux_tot) + config.mag_zeropoint_ref
+                select_mag = (mag_tot >= config.mag_brightest_ref) & (
+                    mag_tot <= config.mag_faintest_ref)
+            else:
+                select_mag = np.isfinite(flux_tot)
+            if select_ref is None:
+                select_ref = select_mag
+            else:
+                select_ref &= select_mag
+
+        if config.coords_ref_to_convert:
+            ra_ref, dec_ref = [catalog_ref[column] for column in config.coords_ref_to_convert.keys()]
+            factor = config.coords_ref_factor
+            radec_true = [geom.SpherePoint(ra*factor, dec*factor, geom.degrees)
+                          for ra, dec in zip(ra_ref, dec_ref)]
+            xy_true = wcs.skyToPixel(radec_true)
+
+            for idx_coord, column_out in enumerate(config.coords_ref_to_convert.values()):
+                catalog_ref[column_out] = np.array([xy[idx_coord] for xy in xy_true])
+
+        select_additional = (len(config.columns_target_select_true)
+                             + len(config.columns_target_select_false)) > 0
+        if select_additional:
+            if select_target is None:
+                select_target = np.ones(len(catalog_target), dtype=bool)
+            for column in config.columns_target_select_true:
+                select_target &= catalog_target[column].values
+            for column in config.columns_target_select_false:
+                select_target &= ~catalog_target[column].values
+
+        logger.info('Beginning MatcherProbabilistic.match with %d/%d ref sources selected vs %d/%d target',
+                    np.sum(select_ref), len(select_ref), np.sum(select_target), len(select_target))
+
+        catalog_out_ref, catalog_out_target, exceptions = self.matcher.match(
+            catalog_ref,
+            catalog_target,
+            select_ref=select_ref,
+            select_target=select_target,
+            logger=logger,
+            logging_n_rows=logging_n_rows,
+        )
+
+        return catalog_out_ref, catalog_out_target, exceptions
+
+    @utils.timer.timeMethod
+    def run(
+        self,
+        catalog_ref: pd.DataFrame,
+        catalog_target: pd.DataFrame,
+        wcs: afwGeom.SkyWcs = None,
+        **kwargs,
+    ) -> pipeBase.Struct:
+        """Match sources in a reference tract catalog with a target catalog.
+
+        Parameters
+        ----------
+        catalog_ref : `pandas.DataFrame`
+            A reference catalog to match objects/sources from.
+        catalog_target : `pandas.DataFrame`
+            A target catalog to match reference objects/sources to.
+        wcs : `lsst.afw.image.SkyWcs`
+            A coordinate system to convert catalog positions to sky coordinates.
+            Only needed if `config.coords_ref_to_convert` is used to convert
+            reference catalog sky coordinates to pixel positions.
+        kwargs : Additional keyword arguments to pass to `match`.
+
+        Returns
+        -------
+        retStruct : `lsst.pipe.base.Struct`
+            A struct with output_ref and output_target attribute containing the
+            output matched catalogs, as well as a dict
+        """
+        catalog_ref, catalog_target, exceptions = self.match(catalog_ref, catalog_target, wcs=wcs, **kwargs)
+        return pipeBase.Struct(cat_output_ref=catalog_ref, cat_output_target=catalog_target,
+                               exceptions=exceptions)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.matcher = MatcherProbabilistic(self.config)
---
+++ tests/test_match_probabilistic_task.py
@@ -0,0 +1,117 @@
+# This file is part of meas_astrom.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import unittest
+import lsst.utils.tests
+
+import lsst.afw.geom as afwGeom
+from lsst.meas.astrom import MatchProbabilisticConfig, MatchProbabilisticTask
+
+import numpy as np
+import pandas as pd
+
+
+class MatchProbabilisticTaskTestCase(lsst.utils.tests.TestCase):
+    """MatchProbabilisticTask test case."""
+    def setUp(self):
+        ra = np.array([-0.1, -0.2, 0., 0.1, 0.2])
+        dec = np.array([-0.15, 0.15, 0, 0.15, -0.15])
+        mag_g = np.array([23., 24., 25., 25.5, 26.])
+        mag_r = mag_g + [0.5, -0.2, -0.8, -0.5, -1.5]
+        zeropoint = MatchProbabilisticConfig.mag_zeropoint_ref.default
+        fluxes = tuple(-0.4*10**(mag - zeropoint) for mag in (mag_g, mag_r))
+        eps_coord = np.full_like(ra, lsst.geom.Angle(0.2, lsst.geom.arcseconds).asDegrees())
+        eps_flux = np.full_like(eps_coord, 10)
+        flags = np.ones_like(eps_coord, dtype=bool)
+
+        columns_flux = ['flux_g', 'flux_r']
+        columns_ref_meas = [
+            MatchProbabilisticConfig.column_ref_coord1.default,
+            MatchProbabilisticConfig.column_ref_coord2.default,
+        ] + columns_flux
+
+        data_ref = {
+            columns_ref_meas[0]: ra[::-1],
+            columns_ref_meas[1]: dec[::-1],
+            columns_flux[0]: fluxes[0][::-1],
+            columns_flux[1]: fluxes[1][::-1],
+        }
+        self.catalog_ref = pd.DataFrame(data=data_ref)
+
+        columns_target_meas = [
+            MatchProbabilisticConfig.column_target_coord1.default,
+            MatchProbabilisticConfig.column_target_coord2.default,
+        ] + columns_flux
+        columns_target_err = [f'{column}Err' for column in columns_target_meas]
+
+        data_target = {
+            columns_target_meas[0]: ra + eps_coord,
+            columns_target_meas[1]: dec + eps_coord,
+            f'{columns_target_meas[0]}Err': eps_coord,
+            f'{columns_target_meas[1]}Err': eps_coord,
+            columns_flux[0]: fluxes[0] + eps_flux,
+            columns_flux[1]: fluxes[1] - eps_flux,
+            f'{columns_flux[0]}Err': eps_flux,
+            f'{columns_flux[1]}Err': eps_flux,
+            MatchProbabilisticConfig.columns_target_select_true.default[0]: flags,
+            MatchProbabilisticConfig.columns_target_select_false.default[0]: ~flags,
+        }
+        self.catalog_target = pd.DataFrame(data=data_target)
+
+        self.task = MatchProbabilisticTask(config=MatchProbabilisticConfig(
+            columns_ref_flux=columns_flux,
+            columns_ref_meas=columns_ref_meas,
+            columns_target_meas=columns_target_meas,
+            columns_target_err=columns_target_err,
+        ))
+        self.wcs = afwGeom.makeSkyWcs(crpix=lsst.geom.Point2D(9000, 9000),
+                                      crval=lsst.geom.SpherePoint(180., 0., lsst.geom.degrees),
+                                      cdMatrix=afwGeom.makeCdMatrix(scale=0.2*lsst.geom.arcseconds))
+
+    def tearDown(self):
+        del self.catalog_ref
+        del self.catalog_target
+        del self.task
+        del self.wcs
+
+    def test_MatchProbabilisticTask(self):
+        result = self.task.run(
+            catalog_ref=self.catalog_ref,
+            catalog_target=self.catalog_target,
+            wcs=self.wcs,
+            logging_n_rows=2,
+        )
+        indices_target = list(result.cat_output_target.match_row.values)
+        self.assertEqual(indices_target, list(np.arange(len(indices_target))[::-1]))
+
+
+class MemoryTester(lsst.utils.tests.MemoryTestCase):
+    pass
+
+
+def setup_module(module):
+    lsst.utils.tests.init()
+
+
+if __name__ == "__main__":
+    lsst.utils.tests.init()
+    unittest.main()
