b96c0c99fe4b4ba6a5cb57bb1a694bc40d75eb38
Dan Taranu <dtaranu@astro.princeton.edu>
2024-09-26 13:52:23 -0700

    Add missing classes to __all__

--- python/lsst/meas/astrom/matcher_probabilistic.py
+++ python/lsst/meas/astrom/matcher_probabilistic.py
@@ -19,7 +19,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__all__ = ['ConvertCatalogCoordinatesConfig', 'MatchProbabilisticConfig', 'MatcherProbabilistic']
+__all__ = [
+    "CatalogExtras", "ComparableCatalog", "ConvertCatalogCoordinatesConfig", "MatchProbabilisticConfig",
+    "MatcherProbabilistic",
+]
 
 from dataclasses import dataclass
 import logging
