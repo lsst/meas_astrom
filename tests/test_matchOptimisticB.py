3dbabf3b674500c2bb3bb960072c6e01873ae222
John Parejko <parejkoj@uw.edu>
2025-04-09 22:10:41 -0700

    Remove line referring to removed selector

--- tests/test_matchOptimisticB.py
+++ tests/test_matchOptimisticB.py
@@ -85,7 +85,6 @@ class TestMatchOptimisticB(unittest.TestCase):
         # This field isn't in the old test catalog.
         tempConfig.sourceSelector["science"].flags.bad.remove("base_PixelFlags_flag_nodata")
         tempConfig.matcher.retarget(measAstrom.MatchOptimisticBTask)
-        tempConfig.sourceSelector["matcher"].excludePixelFlags = False
         tempSolver = measAstrom.AstrometryTask(config=tempConfig, refObjLoader=None)
         sourceSelection = tempSolver.sourceSelector.run(sourceCat)
 
